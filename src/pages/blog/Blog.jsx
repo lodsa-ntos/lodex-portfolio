f412e499b778883b8d131e4838408014df6640b0
Lodney Santos <a21505293@alunos.ulht.pt>
2025-08-02 14:34:22 +0100

    Update: 2025-08-02 14:34:22 [package-lock.json, package.json, src/pages/blog/Blog.jsx]

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import GlobalContainer from "../../utils/GlobalContainer";
+import { Helmet } from "react-helmet";
 
 function Blog() {
   return (
-    <section
-      id="sobremim"
-      className="min-h-[95vh] pt-20 border-b shadow-md flex items-center justify-center text-center"
-    >
-      <GlobalContainer>
-        <div className="flex flex-col items-center justify-center text-center">
-          <h2 className="text-center text-4xl font-semibold">Conteúdo com propósito. <br className="lg:hidden" />Em construção.</h2>
-          <p className="text-sm text-gray-500 -tracking-wide mt-2">
-            Aqui vou partilhar ideias, reflexões e soluções práticas sobre frontend, design e criação digital.
-          </p>
-        </div>
-      </GlobalContainer>
-    </section>
+    <>
+      <Helmet>
+        <title>Lodex Studio — Blog</title>
+      </Helmet>
+      <section
+        id="sobremim"
+        className="min-h-[95vh] pt-20 border-b shadow-md flex items-center justify-center text-center"
+      >
+        <GlobalContainer>
+          <div className="flex flex-col items-center justify-center text-center">
+            <h2 className="text-center text-4xl font-semibold">
+              Conteúdo com propósito. <br className="lg:hidden" />
+              Em construção.
+            </h2>
+            <p className="text-sm text-gray-500 -tracking-wide mt-2">
+              Aqui vou partilhar ideias, reflexões e soluções práticas sobre
+              frontend, design e criação digital.
+            </p>
+          </div>
+        </GlobalContainer>
+      </section>
+    </>
   );
 }
 
