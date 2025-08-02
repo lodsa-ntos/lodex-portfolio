36bae528b8fdf8574ca6f25dc45275148ba10c92
Lodney Santos <a21505293@alunos.ulht.pt>
2025-08-02 15:02:01 +0100

    Update: 2025-08-02 15:02:01 [src/pages/LandingPage.jsx]

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,11 +5,18 @@ import ExpertiseSection from "../components/ExpertiseSection";
 import GlobalContainer from "../utils/GlobalContainer";
 import AboutSection from "../components/AboutSection";
 import WorksSection from "../components/WorksSection";
-
+import { Helmet } from "react-helmet";
 
 function LandingPage() {
   return (
     <>
+      <Helmet>
+        <title>LodeX Studio | Desenvolvimento Web e UI/UX</title>
+        <meta
+          name="description"
+          content="Portfólio da Lodex Studio. Projetos e serviços de frontend com foco em clareza, estrutura e experiências digitais escaláveis."
+        ></meta>
+      </Helmet>
       <div className="shadow bg-gradient-to-b from-[#f4f8ff] via-white to-white">
         <SectionContainer>
           <Hero />
