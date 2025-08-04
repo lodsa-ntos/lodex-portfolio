38de474bca150cc33731f0d2601ac3b2cb37e89e
Lodney Santos <a21505293@alunos.ulht.pt>
2025-08-04 21:26:54 +0100

    Update: 2025-08-04 21:26:54 [src/pages/NotFound.jsx]

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import { BubblyLink } from "../library/BubblyLink";
+import { Helmet } from "react-helmet";
 
 function NotFound() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-4 text-center">
-      <h1 className="text-5xl font-bold bg-secundario text-white mb-4">404</h1>
-      <p className="text-lg">A página que procuras não existe.</p>
-      <BubblyLink
-        to="/"
-        colorStart="#004AAD"
-        colorEnd="#FFFFFF"
-        className="mt-6 text-blue-600 underline hover:text-blue-800 transition duration-500"
-      >
-        Voltar ao início
-      </BubblyLink>
-    </div>
+    <>
+      <Helmet>
+        <title>Página não encontrada | LodeX Studio</title>
+        <meta
+          name="description"
+          content="A página que procuras não existe ou foi movida. Volta à página inicial ou explora os projetos disponíveis na LodeX Studio."
+        />
+      </Helmet>
+      
+      <div className="min-h-screen flex flex-col items-center justify-center px-4 text-center">
+        <h1 className="text-5xl font-bold bg-secundario text-white mb-4">
+          404
+        </h1>
+        <p className="text-lg">A página que procuras não existe.</p>
+        <BubblyLink
+          to="/"
+          colorStart="#004AAD"
+          colorEnd="#FFFFFF"
+          className="mt-6 text-blue-600 underline hover:text-blue-800 transition duration-500"
+        >
+          Voltar ao início
+        </BubblyLink>
+      </div>
+    </>
   );
 }
 
