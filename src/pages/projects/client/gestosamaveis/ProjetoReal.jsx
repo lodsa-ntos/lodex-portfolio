762eabd714459e073af248ca1e6a08e3a22ac8f2
Lodney Santos <a21505293@alunos.ulht.pt>
2025-08-05 11:21:55 +0100

    Update: 2025-08-05 11:21:55 [src/pages/projects/client/gestosamaveis/ProjetoReal.jsx]

--- src/pages/projects/client/gestosamaveis/ProjetoReal.jsx
+++ src/pages/projects/client/gestosamaveis/ProjetoReal.jsx
@@ -7,6 +7,9 @@ import { TbContract } from "react-icons/tb";
 import { FaReact } from "react-icons/fa6";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { TbBrandFramerMotion } from "react-icons/tb";
+import { RxDrawingPinFilled } from "react-icons/rx";
+import { AiOutlineFileProtect } from "react-icons/ai";
+import { LiaUniversitySolid } from "react-icons/lia";
 
 function ProjetoReal() {
   return (
@@ -23,7 +26,7 @@ function ProjetoReal() {
 
       <section
         id="gestosamaveis"
-        className="min-h-screen pt-36 border-b shadow-md transition-all duration-500"
+        className="min-h-screen pt-36 transition-all duration-500"
       >
         <GlobalContainer>
           <div className="grid grid-cols-[1fr_480px] gap-[4rem] py-16">
@@ -63,24 +66,24 @@ function ProjetoReal() {
             {/** Explicação */}
             <div className="sticky top-24 self-start h-fit">
               <h1 className="pb-2 text-[#0E1011] text-xl sm:text-2xl md:text-3xl lg:text-4xl font-medium">
-                Gestos Amáveis 💚
+                Gestos Amáveis
               </h1>
 
               <div className="flex items-center justify-start gap-1.5">
                 <div className="ring-1 ring-inset bg-green-50 text-[#2F9B92] ring-[#2F9B92] text-xs px-2 py-1 rounded-full flex items-center justify-end text-end gap-1 w-fit">
-                <TbContract />
-                Projeto real - Cliente
-              </div>
+                  <TbContract />
+                  Projeto real - Cliente
+                </div>
 
-              <FaReact title="React.js" className="text-blue-500" />
-              <RiTailwindCssFill
-                title="Tailwind CSS"
-                className="text-sky-400"
-              />
-              <TbBrandFramerMotion
-                title="Framer Motion"
-                className="text-purple-500"
-              />
+                <FaReact title="React.js" className="text-blue-500" />
+                <RiTailwindCssFill
+                  title="Tailwind CSS"
+                  className="text-sky-400"
+                />
+                <TbBrandFramerMotion
+                  title="Framer Motion"
+                  className="text-purple-500"
+                />
               </div>
 
               <h3 className="mb-6 mt-6 text-gray-700 text-lg leading-relaxed">
@@ -103,6 +106,123 @@ function ProjetoReal() {
                 própria cliente Foi um projeto entregue com autonomia e
                 compromisso, da estrutura até os detalhes finais.
               </p>
+
+              <div className="mb-6">
+                <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
+                  <h1 className="flex items-center justify-start gap-1 text-[#6a7282] font-[500] text-sm">
+                    <RxDrawingPinFilled className="text-[#2F9B92]"/> Tecnologias e Estrutura
+                  </h1>
+                  <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span class="size-5 flex justify-center items-center rounded-full bg-green-50 text-[#2F9B92]">
+                      <svg
+                        class="shrink-0 size-3.5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <polyline points="20 6 9 17 4 12"></polyline>
+                      </svg>
+                    </span>
+                    <span className="text-sm">
+                      Responsivo em todos os dispositivos
+                    </span>
+                  </li>
+
+                  <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span class="size-5 flex justify-center items-center rounded-full bg-green-50 text-[#2F9B92]">
+                      <svg
+                        class="shrink-0 size-3.5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <polyline points="20 6 9 17 4 12"></polyline>
+                      </svg>
+                    </span>
+                    <span className="text-sm">
+                      Navegação simples e intuitiva
+                    </span>
+                  </li>
+
+                  <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span class="size-5 flex justify-center items-center rounded-full bg-green-50 text-[#2F9B92]">
+                      <svg
+                        class="shrink-0 size-3.5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <polyline points="20 6 9 17 4 12"></polyline>
+                      </svg>
+                    </span>
+                    <span className="text-sm">
+                      Layout pensado para escalabilidade
+                    </span>
+                  </li>
+
+                  <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span class="size-5 flex justify-center items-center rounded-full bg-green-50 text-[#2F9B92]">
+                      <svg
+                        class="shrink-0 size-3.5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <polyline points="20 6 9 17 4 12"></polyline>
+                      </svg>
+                    </span>
+                    <span className="text-sm">
+                      Código modular e reutilizável
+                    </span>
+                  </li>
+
+                  <li className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span class="size-5 flex justify-center items-center rounded-full bg-green-50 text-[#2F9B92]">
+                      <svg
+                        class="shrink-0 size-3.5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <polyline points="20 6 9 17 4 12"></polyline>
+                      </svg>
+                    </span>
+                    <span className="text-sm">
+                      Integração futura com CMS possível
+                    </span>
+                  </li>
+                </ul>
+              </div>
 
               <button
                 className="bg-primario font-semibold text-white py-3 px-3 rounded-full shadow-md hover:shadow-lg text-sm w-full sm:w-fit max-w-xs hover:bg-secundario transition-all duration-500 whitespace-nowrap hover:ring-2 hover:ring-secundario hover:scale-105 hover:ring-inset flex items-center gap-1"
@@ -127,6 +247,114 @@ function ProjetoReal() {
                     Gestos Amáveis
                   </p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </GlobalContainer>
+      </section>
+
+      {/** FIM - Mais do portfólio */}
+      <section className="shadow-lg border-b mt-20 mb-20 lg:mb-0 lg:mt-4 lg:py-16 bg-gray-100">
+        <GlobalContainer>
+          <div className="flex flex-row items-center justify-center mb-6 gap-x-1">
+            {/* Top Line */}
+            <div className="w-full h-[1px] rounded-full bg-lines-color"></div>
+
+            {/* Main title */}
+            <h1
+              id="projetos"
+              className="text-sm w-[50%] mx-auto uppercase font-bold text-[#777] tracking-wider text-center"
+            >
+              Você também pode gostar
+            </h1>
+
+            {/* Top Line */}
+            <div className="w-full mx-auto h-[1px] rounded-full bg-lines-color "></div>
+          </div>
+
+          {/* Cards portfolio */}
+          <div className="max-w-content mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {/** Card 1 */}
+            <div class="cursor-pointer group relative flex flex-col my-6 mx-auto bg-white shadow border border-slate-200 rounded-lg w-full max-w-sm hover:shadow-lg transition-shadow duration-500">
+              <div class="relative h-56 m-2.5 overflow-hidden text-white rounded-md">
+                {/** Badge */}
+                <div relative>
+                  <div className="absolute top-3 right-4 rounded-full p-1.5 tracking-tight ring-1 ring-inset bg-gray-50 text-gray-600 ring-purple-700/10 z-50 flex items-center gap-2">
+                    <a
+                      href="#preview"
+                      className="flex gap-1 items-center text-xs font-medium font-Satoshi transition-transform duration-500 ease-in-out"
+                    >
+                      <LiaUniversitySolid /> Projeto académico
+                    </a>
+                  </div>
+                </div>
+
+                <img
+                  class="transition-transform duration-500 ease-[cubic-bezier(0.25, 1, 0.5, 1)] transform group-hover:scale-110"
+                  src="images/portfolio/android.svg"
+                  alt="biblioteca-escolar-camarate"
+                />
+              </div>
+              <div class="p-4">
+                <h6 class="mb-2 text-slate-800 text-xl font-semibold">
+                  Biblioteca Escolar Camarate
+                </h6>
+                <p class="text-slate-600 leading-normal font-light">
+                  Aplicação mobile desenvolvida como projeto final de curso, com
+                  base numa colaboração real com a Escola Secundária de
+                  Camarate.
+                </p>
+              </div>
+              <div class="px-4 pb-4 pt-0 mt-2">
+                <a href="/academico" className="transition-all duration-500">
+                  <button
+                    class="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none duration-500"
+                    type="button"
+                  >
+                    Ver caso de estudo
+                  </button>
+                </a>
+              </div>
+            </div>
+
+            {/** Card 3 */}
+            <div class="md:hidden cursor-pointer group relative lg:flex flex-col my-6 mx-auto bg-white shadow border border-slate-200 rounded-lg w-full max-w-sm hover:shadow-lg transition-shadow duration-500">
+              <div class="relative h-56 m-2.5 overflow-hidden text-white rounded-md">
+                {/** Badge */}
+                <div className="relative">
+                  <div className="absolute top-3 right-4 rounded-full p-1.5 tracking-tight ring-1 ring-inset bg-blue-50 text-[#3b82f6] ring-blue-700/10 z-50 flex items-center gap-2">
+                    <a
+                      href="#preview"
+                      className="flex gap-1 items-center text-xs font-medium font-Satoshi transition-transform duration-200 ease-in-out"
+                    >
+                      <AiOutlineFileProtect className="text-blue-600" />{" "}
+                      Conceito pessoal
+                    </a>
+                  </div>
+                </div>
+
+                <img
+                  class="transition-transform duration-500 ease-[cubic-bezier(0.25, 1, 0.5, 1)] transform group-hover:scale-110"
+                  src="images/portfolio/launchPeack.svg"
+                  alt="investment-seed-round"
+                />
+              </div>
+              <div class="p-4">
+                <h6 class="mb-2 text-slate-800 text-xl font-semibold">
+                  LaunchPeack
+                </h6>
+                <p class="text-slate-600 leading-normal font-light">
+                  Landing page criada como conceito para uma solução fictícia.
+                  Feita com React, Tailwind CSS e Framer Motion.
+                </p>
+              </div>
+              <div class="px-4 pb-4 pt-0 mt-2">
+                <button
+                  class="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  type="button"
+                >
+                  Ver caso de estudo
+                </button>
               </div>
             </div>
           </div>
