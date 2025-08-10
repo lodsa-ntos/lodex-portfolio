2bf010fc5db5ae9d32099a9ee2d72e86ada18549
Lodney Santos <a21505293@alunos.ulht.pt>
2025-08-10 01:07:40 +0100

    Update: 2025-08-10 01:07:40 [public/images/blog/office1.avif, public/images/blog/office2.avif, public/images/blog/office3.jpg, public/images/blog/office4.jpg, src/pages/blog/Blog.jsx]

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -14,20 +14,29 @@ function Blog() {
       </Helmet>
       <section
         id="sobremim"
-        className="min-h-[95vh] pt-20 border-b shadow-md flex items-center justify-center text-center"
+        className="relative min-h-[95vh] pt-20 border-b shadow-md overflow-hidden bg-[url(images/blog/office4.jpg)] w-full h-[250px] bg-cover bg-center bg-no-repeat mb-8 md:h-[350px]"
       >
-        <GlobalContainer>
-          <div className="flex flex-col items-center justify-center text-center">
-            <h2 className="text-center text-4xl font-semibold">
-              Conteúdo com propósito. <br className="lg:hidden" />
-              Em construção.
-            </h2>
-            <p className="text-sm text-gray-500 -tracking-wide mt-2">
-              Aqui vou partilhar ideias, reflexões e soluções práticas sobre
-              frontend, design e criação digital.
+        <div className="absolute inset-0 bg-gradient-to-t to-gray-100/40 via-gray-600/20 from-transparent"></div>
+
+        <div className="absolute inset-0 bg-gradient-to-b to-gray-900/40 via-gray-900/20 from-transparent"></div>
+
+        <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/50 to-transparent backdrop-blur-[2px] text-white py-5 space-y-2 text-left">
+          <GlobalContainer>
+            <h1 className="mb-1 lg:text-3xl">Insights</h1>
+            <h3
+              id="card1-title"
+              className="text-base sm:text-4xl md:text-5xl lg:text-6xl font-semibold mb-3 leading-relaxed"
+            >
+              Tutoriais e ideias para <br />acelerar o seu código.
+            </h3>
+            <p className="text-base max-w-3xl leading-relaxed">
+              Dicas práticas, guias passo a passo e exemplos reais para ajudar
+              programadores e designers a criarem projetos mais rápidos, bonitos
+              e eficientes. Conteúdo focado em React, TailwindCSS, UI/UX e
+              ferramentas modernas para web.
             </p>
-          </div>
-        </GlobalContainer>
+          </GlobalContainer>
+        </div>
       </section>
     </>
   );
