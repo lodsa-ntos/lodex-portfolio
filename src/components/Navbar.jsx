843bf2660cc7dc05141231016e631d8f0f120de9
Lodney Santos <a21505293@alunos.ulht.pt>
2025-08-27 19:13:06 +0100

    Update: 2025-08-27 19:13:05 [src/components/Navbar.jsx]

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -211,7 +211,7 @@ function Navbar() {
                 <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
                 <span class="relative inline-flex size-3 rounded-full bg-green-500"></span>
               </span>
-              Open to collaborations
+              Let’s build together
             </small>
             {/* Social media */}
             <div className="hidden lg:flex gap-x-4 items-center pr-6">
